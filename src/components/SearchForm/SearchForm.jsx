008df6ffebc42ccfa72d74f7174773ecdfe6ce52
MertArar <85193329+MertArar@users.noreply.github.com>
2023-06-08 04:20:42 +0300

    searchFrom added

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { FaSearch } from "react-icons/fa";
+
+import "./searchform.css";
 
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  return (
+    <div className="search-form">
+      <div className="container">
+        <div className="search-form-content">
+          <form className="search-form">
+            <div className="search-form-elem flex flex-sb bg-white">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="The Last World..."
+              />
+              <button type="submit" className="flex flex-c">
+                <FaSearch className="text-purple" size={32} />
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchForm;
