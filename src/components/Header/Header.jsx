ad0b074613e55bf599fbc462396e5fe7745ce91a
MertArar <85193329+MertArar@users.noreply.github.com>
2023-06-08 03:50:15 +0300

    general css variables added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import SearchFrom from "../SearchForm/SearchForm";
+import "./header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="holder">
+      <header className="header">
+        <Navbar />
+        <div className="header-content flex flex-c text-center text-white">
+          <h2 className="header-title text-capitalize">
+            find your book choice.
+          </h2>{" "}
+          <br />
+          <p className="header-text fs-18 fw-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
+            fugit dignissimos. Id atque molestias, aut voluptatum mollitia
+            maiores dolore sit molestiae qui, repellat laborum fugit?
+          </p>
+          <SearchFrom />
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default Header;
