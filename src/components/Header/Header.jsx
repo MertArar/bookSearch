3105e12cede5b1340354cb4ce6591e0ea30b206d
MertArar <85193329+MertArar@users.noreply.github.com>
2023-06-08 03:28:02 +0300

    files added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import SearchFrom from "../SearchForm/SearchForm";
+
+const Header = () => {
+  return <div>Header</div>;
+};
+
+export default Header;
